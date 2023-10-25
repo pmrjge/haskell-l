edb1126304fc6b4a7cf530c6f9bf05017131e086
Pedro Jorge <pmrjge@gmail.com>
2023-10-25 15:28:33 +0100

    Refactored project

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,4 @@ module Main where
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
-e :: Double
-e = exp 1
-
-square :: Double -> Double
-square x = x**2
-
-f = \x -> x**3
-h = \x -> 3*x**2 - 4 * x + 5
-g = \x -> cos(2 * x)
-v = \t -> 10 * t + 20
-z = \x -> exp (-x)
-
-
-cosSq :: Double -> Double
-cosSq x = square (cos x)
-
-cosSq' :: Double -> Double
-cosSq' x = square $ cos x
-
-cosSq'' :: Double -> Double
-cosSq'' x = (square . cos) x
-
-cosSq''' :: Double -> Double
-cosSq''' = square . cos
-
 
