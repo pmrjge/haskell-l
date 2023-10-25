d5a85c95891229fcd56c4a2cf980d47508e53981
Pedro Jorge <pmrjge@gmail.com>
2023-10-25 10:28:40 +0100

    Function composition

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 module Main where
+import Data.Bits (Bits(xor))
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
@@ -14,5 +15,18 @@ h = \x -> 3*x**2 - 4 * x + 5
 g = \x -> cos(2 * x)
 v = \t -> 10 * t + 20
 z = \x -> exp (-x)
+
+
+cosSq :: Double -> Double
+cosSq x = square (cos x)
+
+cosSq' :: Double -> Double
+cosSq' x = square $ cos x
+
+cosSq'' :: Double -> Double
+cosSq'' x = (square . cos) x
+
+cosSq''' :: Double -> Double
+cosSq''' = square . cos
 
 
