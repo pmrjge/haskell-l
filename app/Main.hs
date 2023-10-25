aaf74ade9bae702d63fb8da92f9f120c80de3580
Pedro Jorge <pmrjge@gmail.com>
2023-10-25 10:24:51 +0100

    Initial project setup

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+e :: Double
+e = exp 1
+
+square :: Double -> Double
+square x = x**2
+
+f = \x -> x**3
+h = \x -> 3*x**2 - 4 * x + 5
+g = \x -> cos(2 * x)
+v = \t -> 10 * t + 20
+z = \x -> exp (-x)
+
+
