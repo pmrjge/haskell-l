ec671b2dcd6e8c70e13c0e44a64581100fe954f0
Pedro Jorge <pmrjge@gmail.com>
2023-10-26 23:28:14 +0100

    Implementing the basics for a calculator

--- app/TypeSystem.hs
+++ app/TypeSystem.hs
@@ -24,5 +24,49 @@ module TypeSystem where
 
     data OneOfTwo a b = Left a | Right b
 
+
+
+    data Peano = Z | S Peano
+
+    toPeano :: Int -> Peano
+    toPeano 0 = Z
+    toPeano n = S (toPeano $ n-1)
+
+    fromPeano :: Peano -> Int
+    fromPeano Z = 0
+    fromPeano (S p) = succ (fromPeano p)
+
+    eqPeano :: Peano -> Peano -> Bool
+    eqPeano p p' = 
+        case (p,p') of 
+        (Z, Z) -> True
+        (S n, S n') -> eqPeano n n'
+        _ -> False
+
+    addPeano :: Peano -> Peano -> Peano
+    addPeano Z b = b
+    addPeano (S a) b = addPeano a (S b)
+
+    data LList a = Empty | Cons a (LList a)
+
+    toList:: [a] -> LList a
+    toList [] = Empty
+    toList (x:xs) = Cons x (toList xs)
+
+    data Expr = Lit Int | Sub Expr Expr | Add Expr Expr | Mul Expr Expr | Div Expr Expr
+
+    eval :: Expr -> Int
+    eval expr = case expr of
+        Lit num -> num
+        Add a1 a2 -> fn (+) a1 a2
+        Sub a1 a2 -> fn (-) a1 a2
+        Mul a1 a2 -> fn (*) a1 a2
+        Div a1 a2 -> fn div a1 a2
+        where 
+            fn :: (Int -> Int -> Int) -> Expr -> Expr->Int
+            fn op a1 a2 = op (eval a1) (eval a2)
+
     
+
+    -- Functions as values
 
