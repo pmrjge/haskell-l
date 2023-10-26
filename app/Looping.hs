80a163dcebac4b13977bc60eb49aaa411e0b98a7
Pedro Jorge <pmrjge@gmail.com>
2023-10-26 18:49:54 +0100

    Examples of foldl and foldr implementations

--- app/Looping.hs
+++ app/Looping.hs
@@ -1,2 +1,9 @@
 module Looping where
     
+    foldl':: (c->a->c)->c->[a]->c
+    foldl' fn acc lst = if null lst then acc else foldl' fn (fn acc (head lst)) (tail lst)
+
+    foldr' :: (a -> b -> b) -> b -> [a] -> b
+    foldr' fn acc lst = if null lst then acc else fn (head lst) $ foldr' fn acc (tail lst)
+
+    
