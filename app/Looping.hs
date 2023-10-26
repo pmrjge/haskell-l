400e3fa60e7d22407f5ca23459a91d5217c6445f
Pedro Jorge <pmrjge@gmail.com>
2023-10-26 19:08:04 +0100

    Examples using list comprehension

--- app/Looping.hs
+++ app/Looping.hs
@@ -6,4 +6,24 @@ module Looping where
     foldr' :: (a -> b -> b) -> b -> [a] -> b
     foldr' fn acc lst = if null lst then acc else fn (head lst) $ foldr' fn acc (tail lst)
 
-    
+    doubleElems :: [Int] -> [Int]
+    doubleElems nums = if null nums
+        then []
+        else
+            let hd = head nums
+                tl = tail nums
+            in (2 * hd) : doubleElems tl
+
+    doubleElems' :: [Int] -> [Int]
+    doubleElems' = map (2*)
+
+    doubleElems'' :: [Int] -> [Int]
+    doubleElems'' = foldr doubleElem [] 
+        where  
+        doubleElem num ls = (2 * num) : ls
+
+    double :: [Integer]
+    double = [ 2 * number | number <- [0..10]]
+
+    doubleOdds:: [Integer]
+    doubleOdds = [2 * number | number <- [0, 10], odd number]
