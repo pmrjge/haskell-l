6220c211823ecc4a7972fb2bb0ba0e519dda74fa
Pedro Jorge <pmrjge@gmail.com>
2023-10-26 23:06:47 +0100

    Data structures and enums

--- app/TypeSystem.hs
+++ app/TypeSystem.hs
@@ -1,4 +1,28 @@
 module TypeSystem where
     data CustomerInfo = CustomerInfo String String Int Int
 
+    data Customer = Customer {firstName::String, lastName:: String, widgetCount::Int, balance:: Int}
+
+    customerGeorge :: Customer
+    customerGeorge = Customer {balance = 100, lastName="Bird", firstName="George", widgetCount=10}
+
+    customerFactory :: String -> String -> Customer
+    customerFactory fname lname = Customer {firstName=fname, lastName=lname, balance=0, widgetCount=5}
+
+    emptyCart :: Customer -> Customer
+    emptyCart customer = customer { widgetCount=0, balance=0}
+
+    data Direction = North | South | East | West
+
+    data PreferredContactMethod = Email String | TextMessage String | Mail String String String Int
+
+    data Person = C {name :: String, personBalance:: Int} | Employee {name::String, managerName:: String, salary :: Int}
+
+    data Person1 = Cst Customer | P Person
+
+    data Perhaps a = Nothing | Just a
+
+    data OneOfTwo a b = Left a | Right b
+
     
+
